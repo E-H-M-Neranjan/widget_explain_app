dcd239d3d6cbc6fbffeb0ad30ba8cca70b8bc51a
Neranjan Hewage <hewagemedhawa43@gmail.com>
2023-06-13 01:35:44 +0530

    Flutter practice

---
+++ lib/widgets/gridview_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GridViewWidget extends StatelessWidget {
+  const GridViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+        ),
+      ),
+    );
+  }
+}
